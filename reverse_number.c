89a9c514fb3cda8c748a854e23f51fa65b0191e5
jeewan99 <er.jeewantaj@gmail.com>
2026-03-24 00:45:45 +0545

    add reverse number program

---
+++ reverse_number.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+    int main(){
+     
+     int num;
+     int reverse;
+     
+     printf("enter a num: ");
+     scanf("%d",&num);
+     
+     while(num){
+     	int lastnum = num % 10;
+     	num /= 10;
+		reverse = (reverse *10 ) + lastnum;
+	 }
+     
+     printf("%d",reverse);
+     
+    return 0;
+    }
